2f616675214878cb7c7289795fb83f69c7749558
Glenn Haertlein <htack210@live.com>
2026-07-10 18:31:43 -0400

    Create components and import them.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
+import { Header, MainGoal } from "./Parts"
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
+      <MainGoal />
     </div>
   );
 }
---
+++ src/Parts.jsx
@@ -0,0 +1,24 @@
+export function Header() {
+    return (
+        <div id="header">
+            <header>
+                <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+                <h1>React Essentials</h1>
+                <p>
+                    Fundamental React concepts you will need for almost any app you are
+                    going to build!
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export function MainGoal() {
+    return (
+        <div id="MainGoal">
+            <p className="myParagraph">My main goal: Learn React in-depth and from the ground up</p>
+        </div>
+    )
+}
+
+export default Header;
