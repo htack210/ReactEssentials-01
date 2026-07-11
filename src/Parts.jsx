884d8d58e8b1eaf52bd597e7a30b5393d4256d1c
Glenn Haertlein <htack210@live.com>
2026-07-10 20:54:52 -0400

    Add dynamic content to the Header component.

--- src/Parts.jsx
+++ src/Parts.jsx
@@ -1,11 +1,19 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core', 'Boss', 'Killer', 'Key'];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 export function Header() {
+    const descriptor = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+
     return (
         <div id="header">
             <header>
                 <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
                 <h1>React Essentials</h1>
                 <p>
-                    Fundamental React concepts you will need for almost any app you are
+                    {descriptor} React concepts you will need for almost any app you are
                     going to build!
                 </p>
             </header>
@@ -16,7 +24,7 @@ export function Header() {
 export function MainGoal() {
     return (
         <div id="MainGoal">
-            <p className="myParagraph">My main goal: Learn React in-depth and from the ground up</p>
+            <p className="myParagraph">My main goal: Learn React in-depth and from the ground up.</p>
         </div>
     )
 }
