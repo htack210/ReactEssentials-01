69aa4c9671abe123e9fb69a0e1792a7ef1c8a45f
Glenn Haertlein <glenn.hline@outlook.com>
2026-07-13 15:12:09 -0400

    Use maps to generate data.

--- src/App.jsx
+++ src/App.jsx
@@ -21,33 +21,19 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {/* Keeping to show alternative way to render */}
-            <CoreConcept
-              image={CORE_CONCEPTS[0].image}
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-            />
-            {/* Use spreader to get key-value pairs */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+          {CORE_CONCEPTS.map((concept) => (
+            <CoreConcept key={concept.title} {...concept} />
+          ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>
-              Components
-            </TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>
-              JSX
-            </TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>
-              Props
-            </TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>
-              State
-            </TabButton>
+            {Object.keys(EXAMPLES).map((topic) => (
+              <TabButton key={topic} isSelected={selectedTopic === topic} onSelect={() => handleSelect(topic)}>
+                {topic.charAt(0).toUpperCase() + topic.slice(1)}
+              </TabButton>
+            ))}
           </menu>
           {!selectedTopic && <h3>Please select a topic...</h3>}
           {selectedTopic && (<div id="tab-content">
